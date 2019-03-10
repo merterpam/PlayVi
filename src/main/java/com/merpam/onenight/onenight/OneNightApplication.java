8a7b3b97567255c08511e2a664d1aaa522c2f7c4
Mert Erpam <merterpam@users.noreply.github.com>
2019-03-10 11:24:21 +0100

    NOTASK initial commit

---
+++ src/main/java/com/merpam/onenight/onenight/OneNightApplication.java
@@ -0,0 +1,13 @@
+package com.merpam.onenight.onenight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneNightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneNightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/merpam/onenight/onenight/OneNightApplicationTests.java
@@ -0,0 +1,16 @@
+package com.merpam.onenight.onenight;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OneNightApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
