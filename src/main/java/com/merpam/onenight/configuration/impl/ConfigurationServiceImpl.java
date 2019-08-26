6d7205255fd8fc12a576df6c0efb9ab87127da09
Mert Erpam <merterpam@users.noreply.github.com>
2019-08-26 22:27:05 +0200

    Add StrSubstitutor for environment variables

--- src/main/java/com/merpam/onenight/configuration/impl/ConfigurationServiceImpl.java
+++ src/main/java/com/merpam/onenight/configuration/impl/ConfigurationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.merpam.onenight.configuration.impl;
 
 import com.merpam.onenight.configuration.ConfigurationService;
+import org.apache.commons.lang.text.StrSubstitutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,9 +26,16 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public void init() {
         try {
             properties.load(propertiesResource.getInputStream());
+            properties.replaceAll((k, v) -> StrSubstitutor.replace(v, System.getenv()));
+            testLANG1055();
         } catch (IOException e) {
             LOG.error("Cannot find properties path", e);
         }
+    }
+
+    public void testLANG1055() {
+        System.setProperty("test_key",  "test_value");
+        final String expected = StrSubstitutor.replace("test_key=${test_key}", System.getProperties());
     }
 
     @Override
