e893d736f53115fe52232e761e282e6cda9ec60a
Mert Erpam <merterpam@users.noreply.github.com>
2021-11-20 13:03:44 +0100

    Update PartyController.java

--- src/main/java/com/merpam/onenight/controller/PartyController.java
+++ src/main/java/com/merpam/onenight/controller/PartyController.java
@@ -18,7 +18,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/party")
-@CrossOrigin(origins = {"http://localhost:3000", "https://one-night-spotify.herokuapp.com", "https://playvi.herokuapp.com", "https://dbilgili.github.io", "https://playvi.app"},
+@CrossOrigin(origins = {"http://localhost:3000", "https://one-night-spotify.herokuapp.com", "https://playvi.herokuapp.com", "https://dbilgili.github.io","https://github.io", "https://playvi.app"},
         allowCredentials = "true",
         exposedHeaders = {SecurityConstants.TOKEN_HEADER})
 public class PartyController {
