4f525d34bf512482a03ed34a61d3c7133339d113
Mert Erpam <merterpam@users.noreply.github.com>
2021-12-27 22:04:48 +0300

    update origins

--- src/main/java/com/merpam/onenight/controller/PartyController.java
+++ src/main/java/com/merpam/onenight/controller/PartyController.java
@@ -5,6 +5,7 @@ import com.merpam.onenight.persistence.facade.PartyFacade;
 import com.merpam.onenight.persistence.facade.UserFacade;
 import com.merpam.onenight.persistence.model.PartyModel;
 import com.merpam.onenight.persistence.model.UserModel;
+import com.merpam.onenight.session.SecurityConstants;
 import com.merpam.onenight.session.SessionUser;
 import com.merpam.onenight.utils.SessionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +18,9 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/party")
-/*@CrossOrigin(origins = {"https://dbilgili.github.io/", "https://dbilgili.github.io", "https://github.io", "https://github.io/"},
+@CrossOrigin(origins = {"https://dbilgili.github.io/", "https://dbilgili.github.io", "https://github.io", "https://github.io/"},
         allowCredentials = "true",
-        exposedHeaders = {SecurityConstants.TOKEN_HEADER})*/
+        exposedHeaders = {SecurityConstants.TOKEN_HEADER})
 public class PartyController {
 
     private PartyFacade partyFacade;
