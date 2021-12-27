ee643d99596cd97e810b78b7f204dc9646a6e0b3
Mert Erpam <merterpam@users.noreply.github.com>
2021-12-27 22:12:12 +0300

    update origins

--- src/main/java/com/merpam/onenight/controller/SongController.java
+++ src/main/java/com/merpam/onenight/controller/SongController.java
@@ -2,15 +2,13 @@ package com.merpam.onenight.controller;
 
 import com.merpam.onenight.spotify.service.SpotifyWebService;
 import com.merpam.onenight.spotify.service.model.SearchTracksResponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/song")
+@CrossOrigin(origins = {"https://dbilgili.github.io"}, allowCredentials = "true")
 public class SongController {
 
     @Resource
